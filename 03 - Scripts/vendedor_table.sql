6644ac890a38691e35c3b2a651874ed7692c6a78
Waler <56610565+walerdaniel@users.noreply.github.com>
2022-10-17 21:16:07 -0300

    Add files via upload

---
+++ 03 - Scripts/vendedor_table.sql
@@ -0,0 +1,9 @@
+create table vendedor_table (
+	matricula VARCHAR(50),
+	nome VARCHAR(50)
+);
+insert into vendedor_table (matricula, nome) values ('337', 'Sax');
+insert into vendedor_table (matricula, nome) values ('721', 'Dael');
+insert into vendedor_table (matricula, nome) values ('560', 'Emilie');
+insert into vendedor_table (matricula, nome) values ('480', 'Jennee');
+insert into vendedor_table (matricula, nome) values ('316', 'Vicky');
